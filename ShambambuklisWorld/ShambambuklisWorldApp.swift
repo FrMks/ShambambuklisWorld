57f735280d842915738f4c58dcf29cb5e62abf2c
m.francuzov <maxfrancuzov@gmail.com>
2024-10-03 14:48:40 +0300

    Initial Commit

---
+++ ShambambuklisWorld/ShambambuklisWorldApp.swift
@@ -0,0 +1,17 @@
+//
+//  ShambambuklisWorldApp.swift
+//  ShambambuklisWorld
+//
+//  Created by Максим Французов on 03.10.2024.
+//
+
+import SwiftUI
+
+@main
+struct ShambambuklisWorldApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
